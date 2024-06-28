61b7c32ce4ec97fbd0dd6be4a2e2dc444305d326
Merna <201927044@std.sci.cu.edu.eg>
2024-06-28 22:16:23 +0300

    AppointmentController Error Handling

--- src/main/java/com/pro/tameit/controllers/AppointmentController.java
+++ src/main/java/com/pro/tameit/controllers/AppointmentController.java
@@ -14,11 +14,19 @@ public class AppointmentController {
     private final AppointmentService appointmentService;
     @PostMapping("/create")
     public ResponseEntity<?> createAppointment(@RequestBody AppointmentDTORequest appointmentDTORequest){
-        return new ResponseEntity<>(appointmentService.createAppointment(appointmentDTORequest), HttpStatus.CREATED);
+        try {
+            return new ResponseEntity<>(appointmentService.createAppointment(appointmentDTORequest), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating appointment: " + e.getMessage());
+        }
     }
     @PatchMapping("/update/{id}")
     public ResponseEntity<?> updateAppointment(@PathVariable Long id, @RequestBody AppointmentDTORequest appointmentDTORequest){
-        return new ResponseEntity<>(appointmentService.updateAppointment(id, appointmentDTORequest), HttpStatus.CREATED);
+        try {
+            return new ResponseEntity<>(appointmentService.updateAppointment(id, appointmentDTORequest), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error update appointment: " + e.getMessage());
+        }
     }
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteAppointment(@PathVariable Long id) {
@@ -33,7 +41,11 @@ public class AppointmentController {
     }
     @PatchMapping("/book/{id}")
     public ResponseEntity<?> bookAppointment(@PathVariable Long id){
-        return new ResponseEntity<>(appointmentService.book(id), HttpStatus.CREATED);
+        try {
+            return new ResponseEntity<>(appointmentService.book(id), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error booking appointment: " + e.getMessage());
+        }
     }
     //Admin & Patient UI((Read Appointments of One Doctor By Doctor ID)):
     @GetMapping("/readDoctorAppointments/{id}")
@@ -74,12 +86,16 @@ public class AppointmentController {
     //Patient UI ((Booking Canceling))
     @PatchMapping("/patientCancelingAppointment/{id}")
     public ResponseEntity<?> patientCancelingAppointment(@PathVariable Long id) {
-        boolean isDeleted = appointmentService.deletePatientFromAppointmentsById(id);
+        try {
+            boolean isDeleted = appointmentService.deletePatientFromAppointmentsById(id);
 
-        if (isDeleted) {
-            return ResponseEntity.ok().body("Patient successfully removed from appointments.");
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No appointments found for the given patient ID.");
+            if (isDeleted) {
+                return ResponseEntity.ok().body("Patient successfully removed from appointments.");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No appointments found for the given patient ID.");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error canceling appointment: " + e.getMessage());
         }
     }
 
