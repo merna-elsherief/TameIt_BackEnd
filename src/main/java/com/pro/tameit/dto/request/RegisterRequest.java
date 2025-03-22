4db0a2464df0585ec3dea9f1340b6b2691b6ae9b
Merna <201927044@std.sci.cu.edu.eg>
2025-03-22 10:23:36 +0200

    We still alive commit

--- src/main/java/com/pro/tameit/dto/request/RegisterRequest.java
+++ src/main/java/com/pro/tameit/dto/request/RegisterRequest.java
@@ -16,19 +16,29 @@ import jakarta.validation.constraints.Size;
 @AllArgsConstructor
 public class RegisterRequest {
 
-    @Size(min = 3, message = "Username must be at least 3 characters long")
     @NotBlank(message = "Username is required")
+    @Size(min = 3, message = "Username must be at least 3 characters long")
     private String userName;
+
     @NotBlank(message = "password is required")
     @Size(min = 6, message = "password must be at least 6 characters long")
     private String password;
+
     @NotBlank(message = "password is required")
     private String confirmPassword;
+
     @Email(message = "Email is not valid")
     @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "first name is required and can't be empty")
+    @Size(min = 2, max = 30,message = "first name must be between 2 -> 30 characters")
     private String firstName;
+
+    @NotBlank(message = "last name is required and can't be empty")
+    @Size(min = 2, max = 30,message = "last name must be between 2 -> 30 characters")
     private String lastName;
+
     private String birthDate;
 
     public boolean isPasswordMatching() {
